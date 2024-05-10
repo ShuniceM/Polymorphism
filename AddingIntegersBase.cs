eb8d4c919406be47750067c68714112618587ea0
smaitland <smaitland@WKS-JHBE4-321>
2024-05-10 08:46:15 +0200

    Add project files.

---
+++ AddingIntegersBase.cs
@@ -0,0 +1,9 @@
+﻿public class AddingIntegersBase
+{
+    public override int MinusNumber(int a, int b, int c)
+    {
+        int val = MinusNumber(a, b, c);
+        return (a - b - c);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        AddingIntegers objAddingIntegers = new AddingIntegers();
+        int retval = objAddingIntegers.CalculatingNumbers(1, 2);
+        Console.WriteLine(retval);
+    }
+}
+
+public class AddingIntegers : AddingIntegersBase
+//One name with different forms
+{
+    public virtual int CalculatingNumbers(int a, int b) //parameters= values that are passed in the object
+    {
+        return a + b; //int/string/bool methods= returns a value
+    }
+}
+
+public class AddingNumerics : AddingIntegers
+{
+    public override int CalculatingNumbers(int a, int b)
+    {
+        int val = CalculatingNumbers(a, b);
+        Console.WriteLine(val);
+        return (a * b);
+
+    }
+
+}
+
+public class SubtractingNumbers:AddingIntegers
+{
+
+}
